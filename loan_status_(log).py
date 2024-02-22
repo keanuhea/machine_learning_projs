9f7d68e510e59c275b1f5252488d8ab68a11d95a
captainwolverine28 <keanuheaparker@gmail.com>
2024-02-21 20:09:10 -0500

    added metrics table

--- loan_status_(log).py
+++ loan_status_(log).py
@@ -135,80 +135,109 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.fit_transform(X_test)
 
+
+metrics = []
+
+
 # Fit Logistic Regression model without regularization
 log_reg = LogisticRegression(penalty='none', solver='saga', random_state=42)
 log_reg.fit(X_train, y_train)
 log_reg_pred = log_reg.predict(X_test)
 log_reg_accuracy = accuracy_score(y_test, log_reg_pred)
-print("Logistic Regression (No Regularization) Accuracy:", log_reg_accuracy)
+log_reg_precision = precision_score(y_test, log_reg_pred)
+log_reg_f1 = f1_score(y_test, log_reg_pred)
+log_reg_roc = roc_auc_score(y_test, log_reg_pred)
+metrics.append(["Logistic Regression", log_reg_accuracy, log_reg_precision, log_reg_f1, log_reg_roc])
 
 # Fit Lasso Logistic Regression model (L1 regularization)
 lasso_log_reg = LogisticRegression(penalty='l1', solver='saga', random_state=42)
 lasso_log_reg.fit(X_train, y_train)
 lasso_log_reg_pred = lasso_log_reg.predict(X_test)
 lasso_log_reg_accuracy = accuracy_score(y_test, lasso_log_reg_pred)
-print("Lasso Logistic Regression Accuracy:", lasso_log_reg_accuracy)
+lasso_reg_precision = precision_score(y_test, lasso_log_reg_pred)
+lasso_reg_f1 = f1_score(y_test, lasso_log_reg_pred)
+lasso_reg_roc = roc_auc_score(y_test, lasso_log_reg_pred)
+metrics.append(["Lasso Logistic Regression", lasso_log_reg_accuracy, lasso_reg_precision, lasso_reg_f1, lasso_reg_roc])
 
 # Fit Ridge Logistic Regression model (L2 regularization)
 ridge_log_reg = LogisticRegression(penalty='l2', solver='saga', random_state=42)
 ridge_log_reg.fit(X_train, y_train)
 ridge_log_reg_pred = ridge_log_reg.predict(X_test)
 ridge_log_reg_accuracy = accuracy_score(y_test, ridge_log_reg_pred)
-print("Ridge Logistic Regression Accuracy:", ridge_log_reg_accuracy)
+ridge_reg_precision = precision_score(y_test, ridge_log_reg_pred)
+ridge_reg_f1 = f1_score(y_test, ridge_log_reg_pred)
+ridge_reg_roc = roc_auc_score(y_test, ridge_log_reg_pred)
+metrics.append(["Ridge Logistic Regression", ridge_log_reg_accuracy, ridge_reg_precision, ridge_reg_f1, ridge_reg_roc])
 
 # Fit Elastic Net Logistic Regression model (L1 and L2 regularization)
 elastic_net_log_reg = LogisticRegression(penalty='elasticnet', solver='saga', l1_ratio=0.5, random_state=42)
 elastic_net_log_reg.fit(X_train, y_train)
 elastic_net_log_reg_pred = elastic_net_log_reg.predict(X_test)
 elastic_net_log_reg_accuracy = accuracy_score(y_test, elastic_net_log_reg_pred)
-print("Elastic Net Logistic Regression Accuracy:", elastic_net_log_reg_accuracy)
-
-print("precision", precision_score(y_test,log_reg_pred))
-print("recall", recall_score(y_test,log_reg_pred))
-print("f1", f1_score(y_test,log_reg_pred))
-print("auc", roc_auc_score(y_test,log_reg_pred))
+elastic_reg_precision = precision_score(y_test, elastic_net_log_reg_pred)
+elastic_reg_f1 = f1_score(y_test, elastic_net_log_reg_pred)
+elastic_reg_roc = roc_auc_score(y_test, elastic_net_log_reg_pred)
+metrics.append(["Elastic Net Logistic Regression", elastic_net_log_reg_accuracy, elastic_reg_precision, elastic_reg_f1, elastic_reg_roc])
 
 #random forest classifier 
 random_forest = RandomForestClassifier(n_estimators=100, random_state=42)
 random_forest.fit(X_train, y_train)
 y_pred_rf = random_forest.predict(X_test)
 accuracy_rf = accuracy_score(y_test, y_pred_rf)
-print("Random Forest Accuracy:", accuracy_rf)
+rf_precision = precision_score(y_test, y_pred_rf)
+rf_f1 = f1_score(y_test, y_pred_rf)
+rf_roc = roc_auc_score(y_test, y_pred_rf)
+metrics.append(['Random Forest', accuracy_rf, rf_precision, rf_f1, rf_roc])
 
 #gradient boosting classifier 
 xgb_classifier = xgb.XGBClassifier()
 xgb_classifier.fit(X_train, y_train)
 y_pred_xgb = xgb_classifier.predict(X_test)
 accuracy_xgb = accuracy_score(y_test, y_pred_xgb)
-print("XGBoost Accuracy:", accuracy_xgb)
+xgb_precision = precision_score(y_test, y_pred_xgb)
+xgb_f1 = f1_score(y_test, y_pred_xgb)
+xgb_roc = roc_auc_score(y_test, y_pred_xgb)
+metrics.append(["Gradient Boosting", accuracy_xgb, xgb_precision, xgb_f1, xgb_roc])
 
 #svm classifier 
 svm_classifier = SVC(kernel='rbf', random_state=42)
 svm_classifier.fit(X_train, y_train)
 y_pred_svm = svm_classifier.predict(X_test)
 accuracy_svm = accuracy_score(y_test, y_pred_svm)
-print("SVM Accuracy:", accuracy_svm)
+svm_precision = precision_score(y_test, y_pred_svm)
+svm_f1 = f1_score(y_test, y_pred_svm)
+svm_roc = roc_auc_score(y_test, y_pred_svm)
+metrics.append(["SVM", accuracy_svm, svm_precision, svm_f1, svm_roc])
 
 #neural networks multi-layer perceptron 
 mlp_classifier = MLPClassifier(hidden_layer_sizes=(100, ), max_iter=1000, random_state=42)
 mlp_classifier.fit(X_train, y_train)
 y_pred_mlp = mlp_classifier.predict(X_test)
 accuracy_mlp = accuracy_score(y_test, y_pred_mlp)
-print("MLP Accuracy:", accuracy_mlp)
+mlp_precision = precision_score(y_test, y_pred_mlp)
+mlp_f1 = f1_score(y_test, y_pred_mlp)
+mlp_roc = roc_auc_score(y_test, y_pred_mlp)
+metrics.append(["Neural Networks Multi-Layer Perceptron", accuracy_mlp, mlp_precision, mlp_f1, mlp_roc])
 
 #k nearest neighbors 
 knn_classifier = KNeighborsClassifier(n_neighbors=5)
 knn_classifier.fit(X_train, y_train)
 y_pred_knn = knn_classifier.predict(X_test)
 accuracy_knn = accuracy_score(y_test, y_pred_knn)
-print("KNN Accuracy:", accuracy_knn)
+knn_precision = precision_score(y_test, y_pred_knn)
+knn_f1 = f1_score(y_test, y_pred_knn)
+knn_roc = roc_auc_score(y_test, y_pred_knn)
+metrics.append(["KNN", accuracy_knn, knn_precision, knn_f1, knn_roc])
 
 #ensemble methods 
 voting_classifier = VotingClassifier(estimators=[('rf', random_forest), ('xgb', xgb_classifier), ('svm', svm_classifier)], voting='hard')
 voting_classifier.fit(X_train, y_train)
 y_pred_voting = voting_classifier.predict(X_test)
 accuracy_voting = accuracy_score(y_test, y_pred_voting)
-print("Voting Classifier Accuracy:", accuracy_voting)
+voting_precision = precision_score(y_test, y_pred_voting)
+voting_f1 = f1_score(y_test, y_pred_voting)
+voting_roc = roc_auc_score(y_test, y_pred_voting)
+metrics.append(["Ensemble", accuracy_voting, voting_precision, voting_f1, voting_roc])
 
 #decision tree classifier 
 dtree = DecisionTreeClassifier(max_depth=3, min_samples_leaf= 35)
@@ -216,4 +245,12 @@ dtree.fit(X_train, y_train)
 y_pred_dtree = dtree.predict(X_test)
 accuracy_dtree = accuracy_score(y_test, y_pred_dtree)
 roc_score_dtree = roc_auc_score(y_test, y_pred_dtree)
-print("Dtree accuracy: ", accuracy_dtree)
+dtree_precision = precision_score(y_test, y_pred_dtree)
+dtree_f1 = f1_score(y_test, y_pred_dtree)
+dtree_roc = roc_auc_score(y_test, y_pred_dtree)
+metrics.append(["Decision Tree", accuracy_dtree, dtree_precision, dtree_f1, dtree_roc])
+
+
+metrics_df = pd.DataFrame(metrics, columns=['Model', 'Accuracy', 'Precision', 'F1', 'AUC'])
+
+print(metrics_df.to_string())
