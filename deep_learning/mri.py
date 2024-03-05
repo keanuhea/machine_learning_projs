7b12afdf86a27f42bcc953636c37282ca8e1cb18
captainwolverine28 <keanuheaparker@gmail.com>
2024-03-05 13:03:26 -0500

    analyzed model

--- deep_learning/mri.py
+++ deep_learning/mri.py
@@ -97,12 +97,6 @@ np.save("labels.npy", labels)
 
 
 
-
-
-
-
-
-
 def plot_activations_multilayer(num_layers, images_per_row, classifier, activations):
     layer_names = []
     for layer in classifier.layers[:num_layers]:
@@ -205,3 +199,70 @@ model.fit(
     epochs=10, 
     batch_size=64
 )
+
+for layer in model.layers:
+    if 'conv2d' in layer.name:
+        kernels, biases = layer.get_weights()
+        print(f"layer name: {layer.name}, num of kernels: {kernels.shape[-1]}, kernel shape: {kernels.shape[:2]}, kernel depth: {kernels.shape[2]}")
+
+for layer in model.layers:
+    if 'conv2d' in layer.name:
+        name = layer.name
+
+        kernels, _ = layer.get_weights()
+        k_min, k_max = kernels.min(), kernels.max()
+        kernels = (kernels - k_min) / (k_max - k_min)
+
+        for i in range(4):
+            kernel = kernels[:,:,:,i]
+            fig = plt.figure(figsize=(5, 2))
+            fig.suptitle(f"{name}, kernel {i+1}", fontsize=15)
+
+            for j in range(3):
+                plt.subplot(1, 3, j+1)
+                plt.imshow(kernel[:,:,j], cmap='gray')
+                plt.xticks([])
+                plt.yticks([])
+for layer in model.layers:
+    if 'conv2d' in layer.name:
+        name = layer.name
+
+        kernels, _ = layer.get_weights()
+        k_min, k_max = kernels.min(), kernels.max()
+        kernels = (kernels - k_min) / (k_max - k_min)
+
+        for i in range(4):
+            kernel = kernels[:,:,:,i]
+            fig = plt.figure(figsize=(5, 2))
+            fig.suptitle(f"{name}, kernel {i+1}", fontsize=15)
+
+            for j in range(3):
+                plt.subplot(1, 3, j+1)
+                plt.imshow(kernel[:,:,j], cmap='gray')
+                plt.xticks([])
+                plt.yticks([])
+
+plt.imshow(kernels[:,:,2,3], cmap='gray')
+plt.show()
+
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = Model(inputs=model.input, outputs=layer_outputs)
+
+# let's pick a sample image 
+img_tensor = images_test[1]
+plt.imshow(np.array(img_tensor, dtype='int'))
+plt.show()
+
+img_tensor = np.expand_dims(img_tensor, axis=0)
+
+activations = activation_model.predict(img_tensor)[:6]
+
+plot_activations_multilayer(7,8,model,activations)
+
+label_map = {"no_tumor": 0, "meningioma_tumor": 1, "glioma_tumor": 2, "pituitary_tumor": 3}
+y = model.predict(img_tensor)
+label = label_map[np.argmax(y)]
+
+plt.imshow(img_tensor.reshape((224,224,3)).astype("uint8"))
+plt.title(f"Predicted class is: {label}", fontsize=13)
+plt.show()
